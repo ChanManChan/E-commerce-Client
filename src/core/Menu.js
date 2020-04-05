905c2bbf4c82b2b6b97acfc873167e83df6260ef
Nanda Gopal <ngopal253@gmail.com>
2020-04-05 10:19:26 +0530

    menu fixed

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,21 +1,21 @@
 import React, { Fragment } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { signout, isAuthenticated } from '../auth';
-import { makeStyles, AppBar, Toolbar, Button, Box } from '@material-ui/core';
+import { makeStyles, AppBar, Toolbar, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   button: {
-    display: 'inline',
-    color: '#fff',
+    marginRight: '.9rem',
   },
 }));
 
 const isActive = (history, path) => {
-  if (history.location.pathname === path) return 'contained';
-  else return 'outlined';
+  if (history.location.pathname === path)
+    return { backgroundColor: '#607d8b', color: '#fff' };
+  else return { backgroundColor: '#f44336', color: '#fff' };
 };
 
 const Menu = ({ history }) => {
@@ -23,49 +23,56 @@ const Menu = ({ history }) => {
   return (
     <div className={classes.root}>
       <AppBar position='static'>
-        <Toolbar>
-          <Box display='flex' justifyContent='flex-end'>
-            <div>
-              <Button
-                component={Link}
-                to='/'
-                className={classes.button}
-                variant={isActive(history, '/')}
-                color='secondary'
-              >
-                Home
-              </Button>
-            </div>
-            <div>
-              <Button
-                component={Link}
-                to='/signin'
-                className={classes.button}
-                variant={isActive(history, '/signin')}
-                color='secondary'
-              >
-                Signin
-              </Button>
-              <Button
-                component={Link}
-                to='/signup'
-                className={classes.button}
-                variant={isActive(history, '/signup')}
-                color='secondary'
-              >
-                Signup
-              </Button>
-              <Button
-                component={Link}
-                to='/'
-                className={classes.button}
-                variant={isActive(history, '/signout')}
-                color='secondary'
-              >
-                Signout
-              </Button>
-            </div>
-          </Box>
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+          <div>
+            <Button
+              component={Link}
+              variant='contained'
+              style={isActive(history, '/')}
+              to='/'
+            >
+              Home
+            </Button>
+          </div>
+          <div>
+            {isAuthenticated() ? (
+              <Fragment>
+                <Button
+                  component={Link}
+                  to='/'
+                  variant='contained'
+                  style={isActive(history, '/signout')}
+                  onClick={() => {
+                    signout(() => {
+                      history.push('/');
+                    });
+                  }}
+                >
+                  Signout
+                </Button>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Button
+                  component={Link}
+                  to='/signin'
+                  variant='contained'
+                  style={isActive(history, '/signin')}
+                  className={classes.button}
+                >
+                  Signin
+                </Button>
+                <Button
+                  component={Link}
+                  variant='contained'
+                  style={isActive(history, '/signup')}
+                  to='/signup'
+                >
+                  Signup
+                </Button>
+              </Fragment>
+            )}
+          </div>
         </Toolbar>
       </AppBar>
     </div>
